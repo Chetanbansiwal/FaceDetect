b068d8d942ba0c26d6fb005d7e2be88f63d8d610
Chetanbansiwal <chetanbansiwal@live.co.uk>
2015-01-22 13:21:20 +0530

    Added Webcam stream recognition

--- face_detect.py
+++ face_detect.py
@@ -1,31 +1,40 @@
+#!/usr/bin/env python
+
 import cv2
 import sys
 
 # Get user supplied values
-imagePath = sys.argv[1]
-cascPath = sys.argv[2]
-
+capture = cv2.VideoCapture(0)
+#imagePath = sys.argv[1]
+cascPath = sys.argv[1]
+win1 = "Faces found"
+cv2.namedWindow(win1)
 # Create the haar cascade
 faceCascade = cv2.CascadeClassifier(cascPath)
 
 # Read the image
-image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+while capture.isOpened():
+	_, image = capture.read()
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Detect faces in the image
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30),
-    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
-)
+	# Detect faces in the image
+	faces = faceCascade.detectMultiScale(
+		gray,
+		scaleFactor=1.1,
+		minNeighbors=5,
+		minSize=(30, 30),
+		flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+	)
 
-print "Found {0} faces!".format(len(faces))
+	#print "Found {0} faces!".format(len(faces))
 
-# Draw a rectangle around the faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+	# Draw a rectangle around the faces
+	#for (x, y, w, h) in faces:
+	#	cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-cv2.imshow("Faces found", image)
-cv2.waitKey(0)
+	cv2.imshow(win1, image)
+	c = cv2.waitKey(1)
+	if int(c)==27:
+		break
+capture.release()
+cv2.destroyAllWindows()	
